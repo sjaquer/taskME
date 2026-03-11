1842916f97bde481ddc0f9bf2065600e1edbf8c9
Sebastián Jaque <jaquesebastian0@gmail.com>
2026-03-11 01:39:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4314282006-26dda",
+  "appId": "1:115195851045:web:d02fe41bcfbda27b9aaacc",
+  "apiKey": "AIzaSyCHDHC4DYrfXCd4tVE9E2WSCKldZsfld4w",
+  "authDomain": "studio-4314282006-26dda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "115195851045"
+};
