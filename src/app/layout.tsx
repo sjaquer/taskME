b667b5e5cdb3c700ddd6f611f1e775ef74f0ae9e
sjaquer <sjaquer@outlook.es>
2026-03-11 12:16:03 -0500

    fix: Eliminar referencia a favicon.ico en la metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,6 @@ export const metadata: Metadata = {
     icon: [
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico', rel: 'shortcut icon' },
     ],
     apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
   },
